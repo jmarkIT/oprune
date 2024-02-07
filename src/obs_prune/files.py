a56af78240b75e19fbb1605fe565fa45e6224422
James Mark <james.david.mark@gmail.com>
2024-02-07 00:39:06 -0600

    Creating vault dataclass and crawl_dir

---
+++ src/obs_prune/files.py
@@ -0,0 +1,23 @@
+import os
+from dataclasses import dataclass
+
+
+@dataclass
+class VaultFile:
+    full_path: str
+    extension: str
+
+    def is_attachment(self) -> bool:
+        return self.extension != "md"
+
+
+def crawl_dir(path: str) -> list[VaultFile]:
+    files = []
+    for root, _, file_names in os.walk(path):
+        for f in file_names:
+            # files.append({"directory": root, "files": file_names})
+            full_path = root + "/" + f
+            extension = f.split(".")[-1]
+            files.append(VaultFile(full_path, extension))
+
+    return files
