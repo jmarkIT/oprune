b5b5a46b84717863d845db79188527b1245a2ae8
James Mark <james.david.mark@gmail.com>
2024-02-07 22:07:56 -0600

    Fixed Imports for pipx

--- src/obs_prune/main.py
+++ src/obs_prune/main.py
@@ -1,5 +1,5 @@
-from files import crawl_dir, delete_file
-from args import set_args
+from obs_prune.files import crawl_dir, delete_file
+from obs_prune.args import set_args
 import platform
 
 
@@ -9,14 +9,14 @@ def main():
     slash = "/"
     if operating_system == "Windows":
         slash = "\\"
-    args = set_args()
-    path = args.vault
-    files = crawl_dir(path, slash)
+    ags = set_args()
+    path = ags.vault
+    all_files = crawl_dir(path, slash)
     attachments = []
-    for file in files:
+    for file in all_files:
         attachments += file.scan_for_attachments()
 
-    for file in files:
+    for file in all_files:
         if file.is_attachment():
             file_name = file.full_path.split(slash)[-1]
             if file_name not in attachments:
