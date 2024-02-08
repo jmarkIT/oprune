a588c86e7eb938b3ba773b041aa60c030271ccbf
James Mark <james.david.mark@gmail.com>
2024-02-08 14:30:04 -0600

    Building out tests

---
+++ tests/test_files.py
@@ -0,0 +1,20 @@
+import pytest
+import platform
+from oprune.files import crawl_dir, VaultFile
+
+os = platform.system()
+
+
+def test_crawl_dir():
+    """Test crawling a directory"""
+    pass
+
+
+def test_scan_for_attachments_one():
+    """Test to detect a single attachment in a file"""
+    pass
+
+
+def test_scan_for_attachments_two():
+    """Test to detect multiple attachments in a file"""
+    pass
