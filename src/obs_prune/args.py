ab8a783ce94332386054eb9b41d5811302708426
James Mark <james.david.mark@gmail.com>
2024-02-07 22:09:53 -0600

    Fixing testing print in args module

--- src/obs_prune/args.py
+++ src/obs_prune/args.py
@@ -5,5 +5,4 @@ def set_args():
     parser = argparse.ArgumentParser(description="Delete unused Obsidian attachments")
     parser.add_argument("vault", type=str, help="The full path to your Obsidian vault")
     args = parser.parse_args()
-    print("arguments!")
     return args
